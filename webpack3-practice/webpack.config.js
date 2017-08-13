81fb95a3779f26bb898d0a149b9c6c00d6aa73e3
yinm <yinm.cminor9@gmail.com>
2017-08-13 16:02:07 +0900

    CSS loader

--- webpack3-practice/webpack.config.js
+++ webpack3-practice/webpack.config.js
@@ -3,5 +3,13 @@ module.exports = {
   output: {
     path: `${__dirname}/dist/`,
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        loaders: ['style-loader', 'css-loader']
+      },
+    ]
   }
 };
