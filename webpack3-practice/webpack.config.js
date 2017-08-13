a99ad8a9d85d761064540328c3ce346be36f6a9f
yinm <yinm.cminor9@gmail.com>
2017-08-13 16:12:23 +0900

    add comments

--- webpack3-practice/webpack.config.js
+++ webpack3-practice/webpack.config.js
@@ -1,12 +1,18 @@
 module.exports = {
+  // webpackでビルドを行う際の、エントリーポイント
   entry: './src/main.js',
+  // バンドルファイルの出力設定
   output: {
+    // 出力先のディレクトリ
     path: `${__dirname}/dist/`,
+    // 出力ファイル名
     filename: 'bundle.js'
   },
   module: {
+    // Loaderの設定
     rules: [
       {
+        // 対象となるファイルの拡張子
         test: /\.css/,
         loaders: ['style-loader', 'css-loader']
       },
